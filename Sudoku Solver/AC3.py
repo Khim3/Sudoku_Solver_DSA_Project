17c18abf8fe4bc99b9a05430fe1a02b79efa50c6
Khim3 <nhatkhiem003@gmail.com>
2024-06-18 23:55:08 +0700

    am am am am am

--- Sudoku Solver/AC3.py
+++ Sudoku Solver/AC3.py
@@ -6,9 +6,10 @@ import queue
 
 
 class AC3Solver:
+    # O(1)
     def __init__(self, csp):
         self.csp = csp
-
+    # O(n^2 * d^3) where n is the number of variables, d is the number of values in the domain
     def AC3(self):
         q = queue.Queue()
         for Xi in self.csp.variables:
@@ -22,7 +23,7 @@ class AC3Solver:
                 for Xk in (self.csp.peers[Xi] - set(Xj)):
                     q.put((Xk, Xi))
         return True
-
+    # O(d^2) where d is the number of values in the domain
     def Revise(self, Xi, Xj):
         revised = False
         values = set(self.csp.values[Xi])
@@ -31,19 +32,19 @@ class AC3Solver:
                 self.csp.values[Xi] = self.csp.values[Xi].replace(x, '')
                 revised = True
         return revised
-
+    # O(n)
     def isConsistent(self, x, Xi, Xj):
         for y in self.csp.values[Xj]:
             if Xj in self.csp.peers[Xi] and y != x:
                 return True
         return False
-
+    # O(n)
     def isComplete(self):
         for variable in squares:
             if len(self.csp.values[variable]) > 1:
                 return False
         return True
-
+    # O(n)
     def write(self, values):
         output = ""
         for variable in squares:
--- Sudoku Solver/Backtrack.py
+++ Sudoku Solver/Backtrack.py
@@ -5,14 +5,15 @@ from copy import deepcopy
 
 
 class BacktrackingSolver:
+    # O(1)
     def __init__(self, csp):
         self.csp = csp
         self.assignment = {}
-
+    # O(1)
     def Backtracking_Search(self, csp_instance):
         self.csp = csp_instance
         return self.Recursive_Backtracking()
-
+    # O(n^d) where n is the number of variables, d is the number of values in the domain
     def Recursive_Backtracking(self):
         if self.isComplete():
             return self.assignment
@@ -34,7 +35,7 @@ class BacktrackingSolver:
                 self.csp.values.update(domain)
 
         return "FAILURE"
-
+    # O(n * d) where n is the number of variables, d is the number of values in the domain
     def Inference(self, inferences, var, value):
         inferences[var] = value
 
@@ -52,22 +53,22 @@ class BacktrackingSolver:
                         return "FAILURE"
 
         return inferences
-
+    # O(n)
     def Select_Unassigned_Variables(self):
         unassigned_variables = dict((squares, len(
             self.csp.values[squares])) for squares in self.csp.values if squares not in self.assignment.keys())
         mrv = min(unassigned_variables, key=unassigned_variables.get)
         return mrv
-
+    # O(n)
     def isComplete(self):
         return set(self.assignment.keys()) == set(squares)
-
+    # O(n)
     def isConsistent(self, var, value):
         for neighbor in self.csp.peers[var]:
             if neighbor in self.assignment.keys() and self.assignment[neighbor] == value:
                 return False
         return True
-
+    # O(n)
     def write(self, values):
         output = ""
         for variable in squares:
